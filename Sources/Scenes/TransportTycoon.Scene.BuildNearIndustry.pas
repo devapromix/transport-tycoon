dcf6c49f8345b10fee11d875a2da9724c4fa2a04
devapromix <maxwof@ukr.net>
2022-08-03 13:59:59 +0300

    Can build truck loading bay near industry.

--- Sources/Scenes/TransportTycoon.Scene.BuildNearIndustry.pas
+++ Sources/Scenes/TransportTycoon.Scene.BuildNearIndustry.pas
@@ -37,12 +37,18 @@ begin
 
   FIndustry := Game.Map.Industry[Game.Map.CurrentIndustry];
   DrawTitle('BUILD IN ' + FIndustry.Name);
-
+  // Dock
   S := '';
   if FIndustry.Dock.Level = 0 then
     S := ' ($' + IntToStr(FIndustry.Dock.Cost) + ')';
   DrawButton(17, 11, FIndustry.Dock.CanBuild(FIndustry.X, FIndustry.Y), 'D',
     'Build Dock' + S);
+  // Truck Loading Bay
+  S := '';
+  if FIndustry.TruckLoadingBay.Level = 0 then
+    S := ' ($' + IntToStr(FIndustry.TruckLoadingBay.Cost) + ')';
+  DrawButton(17, 12, FIndustry.TruckLoadingBay.CanBuild, 'L',
+    'Build Truck Loading Bay' + S);
 
   AddButton(19, 'Esc', 'Close');
 
@@ -68,6 +74,8 @@ begin
         case MY of
           11:
             Key := TK_D;
+          12:
+            Key := TK_L;
         end;
     end;
   end;
@@ -80,6 +88,14 @@ begin
         begin
           FIndustry.Dock.Build;
           Scenes.SetScene(scDock, scIndustry);
+        end;
+      end;
+    TK_L:
+      begin
+        if FIndustry.TruckLoadingBay.CanBuild then
+        begin
+          FIndustry.TruckLoadingBay.Build;
+          Scenes.SetScene(scTruckLoadingBay, scIndustry);
         end;
       end;
   end;
