85d67ca26d81270883ec319f267f9b2cebb42b0f
devapromix <maxwof@ukr.net>
2023-01-24 12:36:16 +0200

    Refactoring.

--- Sources/Scenes/TransportTycoon.Scene.Menu.OpenGame.pas
+++ Sources/Scenes/TransportTycoon.Scene.Menu.OpenGame.pas
@@ -14,7 +14,7 @@ type
 
   public
     procedure Render; override;
-    procedure Update(var Key: Word); override;
+    procedure Update(var AKey: Word); override;
   end;
 
 implementation
@@ -28,33 +28,33 @@ uses
 
 procedure TSceneOpenGameMenu.Render;
 var
-  I: Integer;
+  LSlot: Integer;
 begin
   Game.Map.Draw(Self.ScreenWidth, Self.ScreenHeight);
 
   DrawFrame(10, 6, 60, 18);
   DrawTitle(8, 'OPEN SAVED GAME');
 
-  for I := 0 to 9 do
-    DrawButton(12, I + 10, False, Chr(Ord('A') + I),
+  for LSlot := 0 to 9 do
+    DrawButton(12, LSlot + 10, False, Chr(Ord('A') + LSlot),
       'DRINNINGBRIDGETOWN TRANSPORT / 1965 / 23.07.2022');
 
   AddButton(21, 'Esc', 'Close');
 end;
 
-procedure TSceneOpenGameMenu.Update(var Key: Word);
+procedure TSceneOpenGameMenu.Update(var AKey: Word);
 begin
-  if (Key = TK_MOUSE_LEFT) then
+  if (AKey = TK_MOUSE_LEFT) then
   begin
     if (GetButtonsY = MY) then
     begin
       case MX of
         35 .. 45:
-          Key := TK_ESCAPE;
+          AKey := TK_ESCAPE;
       end;
     end;
   end;
-  case Key of
+  case AKey of
     TK_ESCAPE:
       Scenes.SetScene(scMainMenu);
   end;
